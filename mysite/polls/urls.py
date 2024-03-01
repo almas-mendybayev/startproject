cb97375f5df867eba879fec7c7ebd9b72368b9fa
almas-mendybayev <149047029+almas-mendybayev@users.noreply.github.com>
2024-03-01 23:16:54 +0600

    Update view with new methods, add urls.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"), # polls/1
+    path("<int:question_id>/results/", views.results, name="results"), # polls/1/results/
+    path("<int:question_id>/vote/", views.vote, name="vote"), # polls/1/vote
 ]
